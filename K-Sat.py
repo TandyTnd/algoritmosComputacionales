f08139500ed98e57318acb91b03b7524ec4a5c3a
Alejandro <alejandro-perezg>
2019-10-07 00:04:25 -0500

    Read of the set values added to an array

--- K-Sat.py
+++ K-Sat.py
@@ -1,11 +1,19 @@
-#Funcion para leer el txt
-
+keyNum = " 0"
+satValues = []
 def nombres():
     print("Alejandro Perez Gonzalez     A01746643")
+
+def leerTexto():
+    print("Leyendo....")
+    with open("texto.txt") as lines:
+        for line in lines:
+            if keyNum in line:
+                satValues.append(line)
+    for x in range(len(satValues)):
+        print(satValues[x])
+
+
 def main():
     nombres()
-
-
-
-
+    leerTexto()
 main()
