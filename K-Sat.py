8818ef917218dcac3a2c18b5ba9136d472cfd1d0
tndrdesk <a01654827@itesm.mx>
2019-10-07 20:28:52 -0500

    ::3

--- K-Sat.py
+++ K-Sat.py
@@ -4,15 +4,16 @@ index = []
 file = open("texto.txt", "r")
 def nombres():
     print("Alejandro Perez Gonzalez     A01746643")
+    print("Oscar Zuniga Lara            A01654827")
 
 def leerTexto():
     values=[]
     print("Leyendo....")
     for line in file:
         if keyNum in line:
-            index.append(line)
+            index.append(line.splitlines())
         else:
-            values.append(line)
+            values.append(line.splitlines())
 
 
     for x in range(len(index)):
@@ -22,7 +23,25 @@ def leerTexto():
         print(values[y])
     values.pop(0)
     values.pop(0)
-    print("solo sobra: "+(str(values)))
+
+    print("solo sobra: "+ str (values))
+    values = values[0]
+    values[0] = int(values[0])
+
+    ##print(values)
+    var = values[0]
+
+    ##print(var)
+    values2 =[]
+    for i in range (0, len(str(var))):
+        ##print(str(var)[i])
+        p = str(var)[i]
+
+        if (p == "0"):
+            values2.append(False)
+        elif (p == "1"):
+            values2.append(True)
+    print(values2)
 
     return values
     return index
@@ -31,7 +50,9 @@ def leerTexto():
 
 
 
+
 def main():
     nombres()
     leerTexto()
+
 main()
